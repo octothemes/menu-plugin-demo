303720ec2e355b866bf3872348c2ecda6bd1aeac
Wong Liang Zan <zan@liangzan.net>
2013-08-29 08:48:59 +0800

    committing the source files

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/menu-plugin-demo/"
+http_images_path = "/menu-plugin-demo/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/menu-plugin-demo/fonts"
+css_dir = "public/menu-plugin-demo/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
